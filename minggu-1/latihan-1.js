0e1577ca4bc68229f024334b5cb9cc2298f278cf
HA141User <muhammadhairi0123@gmail.com>
2026-03-07 07:10:16 +0800

    belajar variabel

---
+++ minggu-1/latihan-1.js
@@ -0,0 +1,7 @@
+const tahunLahir = 2006
+let tahunIni = 2026
+let Kota = "Makassar"
+
+console.log('Tahun lahir saya adalah ', tahunLahir)
+console.log('Sekarang adalah tahun ', tahunIni)
+console.log('Kota tempat tinggal saya sekarang adalah : ', Kota)
