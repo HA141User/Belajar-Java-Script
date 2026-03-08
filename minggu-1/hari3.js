37152babe2c679b9d4fa4b2f1ec4ed392124b521
HA141User <muhammadhairi0123@gmail.com>
2026-03-08 13:32:24 +0800

    refactor: restore siswaList initialization and improve formatting

--- minggu-1/hari3.js
+++ minggu-1/hari3.js
@@ -3,12 +3,28 @@ function ambilSemuaNilai(siswaList){
     return semuaNilai;
 }
 
-// const siswaList = [
-//   { nama: "Arif", nilai: 80 },
-//   { nama: "Budi", nilai: 85 },
-//   { nama: "Cici", nilai: 95 },
-//   { nama: "Dudung", nilai: 79 },
-// ];
+const siswaList = [
+  { nama: "Arif", nilai: 80 },
+  { nama: "Budi", nilai: 85 },
+  { nama: "Cici", nilai: 95 },
+  { nama: "Dudung", nilai: 79 },
+];
 
 
-console.log(ambilSemuaNilai(siswaList));
+function filterNilaiMinimal(siswaList, minNilai){
+    const lulus = siswaList.filter(s => s.nilai >= minNilai);
+    return lulus;
+}
+
+function rataRataDenganReduce(siswaList){
+    const total = siswaList.reduce((acc, s) => acc + s.nilai, 0);
+    const rataRata = total / siswaList.length;
+    return rataRata;
+}
+
+console.log(`Semua nilai siswa : ${ambilSemuaNilai(siswaList)}`);
+const minimalNilai = filterNilaiMinimal(siswaList, 80);
+const hasilLulus = minimalNilai.map(s => `${s.nama} dengan nilai ${s.nilai}`).join('\n');
+
+console.log(`Yang lolos nilai 80 ke atas adalah : \n${hasilLulus}`);
+console.log(`Rata rata nilai siswa adalah ${rataRataDenganReduce(siswaList)}`);
