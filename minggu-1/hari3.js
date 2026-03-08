5ac18aa225dfd5d6a425cae65107d4635c1debc8
HA141User <muhammadhairi0123@gmail.com>
2026-03-08 13:33:36 +0800

    refactor: improve formatting and consistency in function definitions

--- minggu-1/hari3.js
+++ minggu-1/hari3.js
@@ -1,6 +1,6 @@
-function ambilSemuaNilai(siswaList){
-    const semuaNilai = siswaList.map(s => s.nilai);
-    return semuaNilai;
+function ambilSemuaNilai(siswaList) {
+  const semuaNilai = siswaList.map((s) => s.nilai);
+  return semuaNilai;
 }
 
 const siswaList = [
@@ -10,21 +10,22 @@ const siswaList = [
   { nama: "Dudung", nilai: 79 },
 ];
 
-
-function filterNilaiMinimal(siswaList, minNilai){
-    const lulus = siswaList.filter(s => s.nilai >= minNilai);
-    return lulus;
+function filterNilaiMinimal(siswaList, minNilai) {
+  const lulus = siswaList.filter((s) => s.nilai >= minNilai);
+  return lulus;
 }
 
-function rataRataDenganReduce(siswaList){
-    const total = siswaList.reduce((acc, s) => acc + s.nilai, 0);
-    const rataRata = total / siswaList.length;
-    return rataRata;
+function rataRataDenganReduce(siswaList) {
+  const total = siswaList.reduce((acc, s) => acc + s.nilai, 0);
+  const rataRata = total / siswaList.length;
+  return rataRata;
 }
 
 console.log(`Semua nilai siswa : ${ambilSemuaNilai(siswaList)}`);
 const minimalNilai = filterNilaiMinimal(siswaList, 80);
-const hasilLulus = minimalNilai.map(s => `${s.nama} dengan nilai ${s.nilai}`).join('\n');
+const hasilLulus = minimalNilai
+  .map((s) => `${s.nama} dengan nilai ${s.nilai}`)
+  .join("\n");
 
 console.log(`Yang lolos nilai 80 ke atas adalah : \n${hasilLulus}`);
 console.log(`Rata rata nilai siswa adalah ${rataRataDenganReduce(siswaList)}`);
