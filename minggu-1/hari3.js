0fcce563fd30474865c4325487de10923bba584c
HA141User <muhammadhairi0123@gmail.com>
2026-03-08 13:37:20 +0800

    refactor: improve formatting and consistency in function definitions

--- minggu-1/hari3.js
+++ minggu-1/hari3.js
@@ -1,31 +1,34 @@
 function ambilSemuaNilai(siswaList) {
-  const semuaNilai = siswaList.map((s) => s.nilai);
-  return semuaNilai;
+    const semuaNilai = siswaList.map((s) => s.nilai);
+    return semuaNilai;
 }
 
 const siswaList = [
-  { nama: "Arif", nilai: 80 },
-  { nama: "Budi", nilai: 85 },
-  { nama: "Cici", nilai: 95 },
-  { nama: "Dudung", nilai: 79 },
+    { nama: "Arif", nilai: 80 },
+    { nama: "Budi", nilai: 85 },
+    { nama: "Cici", nilai: 95 },
+    { nama: "Dudung", nilai: 79 },
+    { nama: "Eka", nilai: 70 }
 ];
 
 function filterNilaiMinimal(siswaList, minNilai) {
-  const lulus = siswaList.filter((s) => s.nilai >= minNilai);
-  return lulus;
+    if(!Array.isArray(siswaList)){ throw new Error("Harus berupa Array")}
+    if(siswaList.length === 0){ throw new Error("Array tidak boleh kosong")}
+    const lulus = siswaList.filter((s) => s.nilai >= minNilai);
+    return lulus;
 }
 
 function rataRataDenganReduce(siswaList) {
-  const total = siswaList.reduce((acc, s) => acc + s.nilai, 0);
-  const rataRata = total / siswaList.length;
-  return rataRata;
+    if(!Array.isArray(siswaList)){ throw new Error("Harus berupa Array")}
+    if(siswaList.length === 0){ throw new Error("Array tidak boleh kosong")}
+    const total = siswaList.reduce((acc, s) => acc + s.nilai, 0);
+    const rataRata = total / siswaList.length;
+    return rataRata;
 }
 
 console.log(`Semua nilai siswa : ${ambilSemuaNilai(siswaList)}`);
 const minimalNilai = filterNilaiMinimal(siswaList, 80);
-const hasilLulus = minimalNilai
-  .map((s) => `${s.nama} dengan nilai ${s.nilai}`)
-  .join("\n");
+const hasilLulus = minimalNilai.map((s) => `${s.nama} dengan nilai ${s.nilai}`).join("\n");
 
 console.log(`Yang lolos nilai 80 ke atas adalah : \n${hasilLulus}`);
 console.log(`Rata rata nilai siswa adalah ${rataRataDenganReduce(siswaList)}`);
