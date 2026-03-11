a8d33a630230892e3c466aa9e1d4ff3cf4779a24
HA141User <muhammadhairi0123@gmail.com>
2026-03-12 00:00:14 +0800

    add fake data

---
+++ minggu-1/lib/data/siswafakedata.js
@@ -0,0 +1,7 @@
+const siswaFakeList = {nama: "budi"};
+const siswaKosong = [];
+const siswaNameNone = [{nama : "", nilai : 89}]
+const siswaSpaceName = [{nama : "   ", nilai : 88}]
+const siswaNotNumber = [{nama : "Budi", nilai : "78"}]
+
+module.exports = { siswaFakeList, siswaKosong, siswaNameNone, siswaSpaceName, siswaNotNumber };
