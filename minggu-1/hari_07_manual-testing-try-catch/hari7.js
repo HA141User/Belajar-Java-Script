ba8a2db2a5673dea5eec5b7e1b4005aad0eae93a
HA141User <muhammadhairi0123@gmail.com>
2026-03-11 23:57:17 +0800

    add: implement manual tests for student validation with various data scenarios

--- minggu-1/hari_07_manual-testing-try-catch/hari7.js
+++ minggu-1/hari_07_manual-testing-try-catch/hari7.js
@@ -1,4 +1,34 @@
 const {siswaList} = require("../lib/data/siswa");
 const {rataRataDenganReduce, ringkasanNilai, validasiSiswaList} = require("../lib/utils/siswa");
+const { siswaFakeList, siswaKosong, siswaNameNone, siswaSpaceName, siswaNotNumber } = require("../lib/data/siswafakedata");
 
-console.log(validasiSiswaList(siswaList));
+function runTest(namaTest, fn){
+    try{
+        fn();
+        console.log(`${namaTest} : OK`);
+    }catch(err){
+        console.log(`${namaTest} : ERROR -> ${err.message}`);
+    }
+}
+
+runTest("validasi siswa list", ()=> validasiSiswaList(siswaList));
+// runTest("validasi siswa list", validasiSiswaList(siswaList));
+
+
+// todo c : test data rusak
+runTest("Testing validasi menggunakan data Rusak", ()=>{
+    validasiSiswaList(siswaFakeList);
+})
+runTest("Testing validasi menggunakan data Rusak", ()=>{
+    validasiSiswaList(siswaKosong);
+})
+runTest("Testing validasi menggunakan data Rusak", ()=>{
+    validasiSiswaList(siswaNameNone);
+})
+runTest("Testing validasi menggunakan data Rusak", ()=>{
+    validasiSiswaList(siswaSpaceName);
+})
+runTest("Testing validasi menggunakan data Rusak", ()=>{
+    validasiSiswaList(siswaNotNumber);
+})
+
